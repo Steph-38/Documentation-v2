438d2b1526252f78938fb0e9619cd502a2079ed9
Steph-38 <44949858+Steph-38@users.noreply.github.com>
2022-04-04 18:51:10 +0200

    Add files via upload

---
+++ Documentation-v2/NodeVueReact/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+await mongoose.connect('mongodb://localhost/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
